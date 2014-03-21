5dd53cfe5fac4fbbfaa821c10b972b540b785e5f
Michael Huntington <mellomike3000@gmail.com>
2014-03-21 16:32:59 -0400

    updating miner stats

--- libs/stats.js
+++ libs/stats.js
@@ -41,10 +41,12 @@ module.exports = function(logger, portalConfig, poolConfigs){
 
     this.getMinerStats = function(address, cback){
 
-        minerStats[coin] = {};
+        var minerStats = {};
         var client = redisClients[0].client;
 
         async.each(redisClients[0].coins, function(coin, cb){
+
+            minerStats[coin] = {};
             
             client.hmget([coin + '_balances'].concat([address]), function(error, results){
                 if (error){
