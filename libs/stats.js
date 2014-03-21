d7e66c33fa7c7fe33aee1cea85cc290250ae5d22
Michael Huntington <mellomike3000@gmail.com>
2014-03-21 02:25:30 -0400

    fixing miner stats bug

--- libs/stats.js
+++ libs/stats.js
@@ -40,12 +40,13 @@ module.exports = function(logger, portalConfig, poolConfigs){
     this.getMinerStats = function(address, cback){
 
         var minerStats = {};
-        var poolConfig = poolConfigs[coin];
-        var internalConfig = poolConfig.shareProcessing.internal;
-        var redisConfig = internalConfig.redis;
-        var client = redis.createClient(redisConfig.port, redisConfig.host);
 
         Object.keys(poolConfigs).forEach(function(coin){
+
+            var poolConfig = poolConfigs[coin];
+            var internalConfig = poolConfig.shareProcessing.internal;
+            var redisConfig = internalConfig.redis;
+            var client = redis.createClient(redisConfig.port, redisConfig.host);
             
             async.waterfall([
 
