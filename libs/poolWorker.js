2a068a055ad6b5c6a923d6f383d3acf9f71d0025
Michael Huntington <mellomike3000@gmail.com>
2014-03-22 02:41:29 -0400

    testing

--- libs/poolWorker.js
+++ libs/poolWorker.js
@@ -32,10 +32,12 @@ module.exports = function(logger){
                     var oldPool = pools[proxyStuff.curActivePool];
                     oldPool.relinquishMiners(
                         function (miner, cback) { 
+                            console.log("______________________________rlinquishMiners__f1_________________");
                             // relinquish miners that are attached to one of the "Auto-switch" ports and leave the others there.
                             cback(typeof(portalConfig.proxy.ports[miner.client.socket.localPort]) !== 'undefined')
                         }, 
                         function (clients) {
+                            console.log("______________________________rlinquishMiners__f1_________________");
                             newCoinPool.attachMiners(clients);
                             proxyStuff.curActivePool = message.coin.toLowerCase();
                         }
