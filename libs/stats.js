7f54e9e442f31e1eb091b977af7a51c9dc0c2ffb
Michael Huntington <mellomike3000@gmail.com>
2014-03-26 09:50:26 -0400

    testing stats from Mintpal

--- libs/stats.js
+++ libs/stats.js
@@ -67,7 +67,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
 
             request('https://api.mintpal.com/market/stats/BC/BTC', function (error, response, body) {
               if (!error && response.statusCode == 200) {
-                console.log(body) // Print the google web page.
+                console.log(body[0].last_price);
               }
             });
 
