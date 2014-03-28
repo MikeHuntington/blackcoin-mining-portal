3910b8920de280bce81dc0987eb7e5bd817104c8
Michael Huntington <mellomike3000@gmail.com>
2014-03-27 20:33:58 -0400

    test

--- libs/website.js
+++ libs/website.js
@@ -150,6 +150,7 @@ module.exports = function(logger){
         if (address != null){
             portalStats.getPayout(address, function(data){
                 res.send(data);
+                res.end();
             });
         }
         else
